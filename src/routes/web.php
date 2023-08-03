5c08b164505bb37ae01c01a6d25a9e62ff0ed5d1
surajshindeappcart <suraj@appcartsystems.com>
2023-08-03 10:57:02 +0530

    version v1.0.7

--- src/routes/web.php
+++ src/routes/web.php
@@ -1,7 +1,7 @@
 <?php
 
 Route::get('project',function(){
-    return "project form route name. an used for test version <b>Hello</b>";
+    return "project form route name. an used for test version <b>Hello</b>"; 
 });
 
 ?>

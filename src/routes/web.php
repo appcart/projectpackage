0549f81879e8a2679e152ff0a3eb6df28ffbb8fd
surajshindeappcart <suraj@appcartsystems.com>
2023-08-07 13:14:47 +0530

    new chanegs

--- src/routes/web.php
+++ src/routes/web.php
@@ -1,7 +1,7 @@
 <?php
 
 Route::get('project',function(){
-    return "project form route name. change on 07 august An used for test and project version <b>Hello v1.1.2</b>"; 
+    return "project form route name. change on 07 august 2023 An used for test and project version <b>Hello v1.1.2</b>"; 
 }); 
 
 ?>
